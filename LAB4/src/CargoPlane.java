6e2bcfac9bc79b7c731aaa5299b964b517c55c74
OleksiiMalanii <146111996+OleksiiMalanii@users.noreply.github.com>
2024-10-31 00:38:41 +0200

    LAB4
    LAB4 1 commit

---
+++ LAB4/src/CargoPlane.java
@@ -0,0 +1,5 @@
+public class CargoPlane extends Airplane {
+    public CargoPlane(String name, double range, int capacity, double payload, double fuelConsumption) {
+        super(name, range, capacity, payload, fuelConsumption);
+    }
+}
---
+++ LAB4/src/PrivateJet.java
@@ -0,0 +1,5 @@
+public class PrivateJet extends Airplane {
+    public PrivateJet(String name, double range, int capacity, double payload, double fuelConsumption) {
+        super(name, range, capacity, payload, fuelConsumption);
+    }
+}
